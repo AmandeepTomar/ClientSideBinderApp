2173e117dc67e01782b918a140b291814ca57cef
TOMAR Amandeep <amandeeptomar005@gmail.com>
2022-01-22 23:02:53 +0530

    - Update service and get random no from another aap , with the help of messenger

--- app/src/main/java/com/amandeep/clientsidebinderapp/MainActivity.kt
+++ app/src/main/java/com/amandeep/clientsidebinderapp/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.amandeep.clientsidebinderapp
 
 import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.IBinder
 import android.util.Log
+import android.widget.Toast
 import com.amandeep.clientsidebinderapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,17 @@ class MainActivity : AppCompatActivity() {
     private  var serviceIntent: Intent?=null
     private var serviceConnection:ServiceConnection?=null
     private val TAG="MainActivity"
+    private var isBind=false
+    private val GET_RANDOM_NO_FLAG=0
+    private var randomNumberValue=0
+
+    //
+    private var randomNoRequestMessenger:Messenger?=null
+
+    // used to handle the reveived message from service
+    private var randomNoReceiveMessenger:Messenger?=null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
@@ -29,10 +41,16 @@ class MainActivity : AppCompatActivity() {
         serviceConnection=object :ServiceConnection{
             override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
                 Log.e(TAG, "onServiceConnected: ${p0?.className} p1 ${p1?.isBinderAlive}")
+                randomNoRequestMessenger= Messenger(p1)
+                randomNoReceiveMessenger= Messenger(ReceiveRandomNumberHandler())
+                isBind=true
             }
 
             override fun onServiceDisconnected(p0: ComponentName?) {
                 Log.e(TAG, "onServiceDisconnected: ${p0?.className}")
+                isBind=false
+                randomNoRequestMessenger=null
+                randomNoReceiveMessenger=null
             }
 
             override fun onBindingDied(name: ComponentName?) {
@@ -50,16 +68,61 @@ class MainActivity : AppCompatActivity() {
 
     private fun handlingClickEvents() {
         binding.btnStartService.setOnClickListener {
-
+            bindServiceHere()
         }
 
         binding.btnStopService.setOnClickListener {
-
+            serviceConnection?.let {
+                    it1 -> unbindService(it1)
+                    isBind=false;
+            }
         }
 
         binding.btnGetRandomNumber.setOnClickListener {
-
+                fetchRandomNumber()
         }
 
     }
+
+    private fun fetchRandomNumber() {
+        if (isBind){
+            val requestMessage=Message.obtain(null,GET_RANDOM_NO_FLAG)
+            requestMessage.replyTo=randomNoReceiveMessenger
+            try {
+                randomNoRequestMessenger?.send(requestMessage)
+            }catch (e : RemoteException){
+                Log.e(TAG, "fetchRandomNumber: $e")
+            }
+        }else{
+            Toast.makeText(this,"Service is not bound yet",Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun bindServiceHere() {
+        serviceConnection?.let { bindService(serviceIntent, it,Context.BIND_AUTO_CREATE) }
+    }
+
+
+    inner class ReceiveRandomNumberHandler : Handler(){
+
+        override fun handleMessage(msg: Message) {
+            randomNumberValue=0;
+            when(msg.what){
+                GET_RANDOM_NO_FLAG->{
+                    randomNumberValue=msg.arg1
+                    binding.tvClientRandomNo.text="Random Number received from Server is $randomNumberValue"
+                }
+            }
+            super.handleMessage(msg)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        isBind=false
+        serviceConnection=null
+    }
+
+
 }
